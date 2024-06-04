18cf260707dc7f4c897b536369ce360ba0ae32b8
Benjamin Kjellgren Kuurne <benjamin.kjellgren.kuurne@iths.se>
2024-06-04 16:36:33 +0200

    changed the connectionstring to be able to be modified with enviroment variables (hopefully)

--- DockerLab1/Program.cs
+++ DockerLab1/Program.cs
@@ -29,9 +29,14 @@ namespace DockerLab1
                 })
                 .AddIdentityCookies();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var server = builder.Configuration["DBServer"] ?? "localhost";
+            var port = builder.Configuration["DBPort"] ?? "1443";
+            var user = builder.Configuration["DBUser"] ?? "SA";
+            var password = builder.Configuration["DBPassword"] ?? "1qasde32S!";
+            var database = builder.Configuration["Database"] ?? "dockerDB";
+            // var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlServer($"Server={server},{port};Initial Catalog={database};User ID={user};Password={password};"));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
